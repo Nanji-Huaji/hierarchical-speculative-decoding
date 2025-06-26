d673ca10cd8c2af0c0ef12e00fb2007b1fc7dbd8
Nanji-Huaji <hua_ji@outlook.com>
2025-06-27 00:35:01 +0800

    准备小改一下下

--- src/debug.py
+++
@@ -1,7 +0,0 @@
-import sys, importlib.util, pkgutil, os
-
-spec = importlib.util.find_spec("numpy")
-print("找到了 numpy spec ->", spec)
-for m in pkgutil.iter_modules():
-    if m.name == "numpy":
-        print("pkgutil 发现 numpy 来自:", m.module_finder)
